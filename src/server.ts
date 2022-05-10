c05763aa8dc9e2625835bcfabe7ca15ad16daaf1
alopsantos <anderson_lops@hotmail.com>
2022-05-10 19:04:42 -0300

    Ajuste de variavel na para a porta de acesso

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,13 @@ import { routes } from "./routes";
 
 const app = express();
 // app.use(cors(
-//   origin: process.env.PUBLIC_SITE_URL || 'http://localhost:3000'
+//   origin: `${process.env.PUBLIC_SITE_URL}` || 'http://localhost:3000'
 // ));
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server running in port 3333");
 });
