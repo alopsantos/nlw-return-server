8168c37d13424d1a10b58d6fc2b1480a6fe5ec75
alopsantos <anderson_lops@hotmail.com>
2022-05-27 11:18:32 -0300

    :bug: Migration com drop na estiquetas

--- prisma/migrations/20220517171109_create_etiquetas/migration.sql
+++ prisma/migrations/20220517171109_create_etiquetas/migration.sql
@@ -1,11 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `etiqueta` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "etiqueta";
 
 -- CreateTable
 CREATE TABLE "etiquetas" (
